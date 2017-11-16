95c1a7e7dab8c9fb3a2e94bae9234adeb71abcfb
Nandan Rao <nandanmarkrao@gmail.com>
2017-11-16 14:25:05 +0100

    Added link from error page to return.

--- src/Error.js
+++ src/Error.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import Button from 'material-ui/Button';
+import history from './history';
+
 export default class Error extends Component {
   render() {
     return (
-      <p> There was a problem with Login. Did you use your UPF email address? Please try again!</p>
+      <div className="error">
+        <p>
+          There was a problem with Login. Did you use your UPF email address? Please try again!
+        </p>
+        <Button raised onClick={ () => window.location.assign('/') }> Return </Button>
+      </div>
     );
   }
 }
